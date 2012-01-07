34425a5bf7b8bced82664b9e020d12da7d39976c
Tomas Corral Casas <amischol@gmail.com>
2012-01-07 14:06:37 +0100

    Fix some issues

--- src/quo.style.js
+++ src/quo.style.js
@@ -15,7 +15,7 @@
                 this.className += ' ' + name;
             }
         });
-    }
+    };
 
     /**
      * ?
@@ -28,7 +28,7 @@
                 this.className = this.className.replace(remove_class, ' ');
             }
         });
-    }
+    };
 
     /**
      * ?
@@ -43,14 +43,14 @@
                 this.className += ' ' + name;
             }
         });
-    }
+    };
 
     /**
      * ?
      */
     $.fn.hasClass = function(name) {
         return _existsClass(name, this[0].className);
-    }
+    };
 
     /**
      * ?
@@ -62,11 +62,11 @@
             this.each(function() {
                 this.style[property] = value;
             });
-    }
+    };
 
     function _existsClass(name, className) {
         var classes = className.split(/\s+/g);
-        return (classes.indexOf(name) >= 0) ? true : false;
+        return (classes.indexOf(name) >= 0);
     }
 
     function _generateRemoveClass(name) {
