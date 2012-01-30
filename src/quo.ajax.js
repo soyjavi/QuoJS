89e074c2bae4c2db638440feeb1f2a3836e20eaa
Philipp Schreiber <ps@schreiber-freunde.de>
2012-01-30 02:31:24 +0100

    Update src/quo.ajax.js

--- src/quo.ajax.js
+++ src/quo.ajax.js
@@ -61,7 +61,9 @@
         }
 
         try {
-            xhr.send(settings.data);
+            var parameters = _serializeParameters(settings.data);
+            parameters = parameters.substr(1, parameters.length);
+            xhr.send(parameters);
             if (xhr.status !== 500) {
                 return (settings.async) ? xhr : _parseResponse(xhr, settings);
             }
