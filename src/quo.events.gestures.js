fed5f37760575b7e47e4e956e0be076e8624b6bc
Javi Jiménez <javi@tapquo.com>
2012-02-04 20:44:50 +0100

    Fixed LongTap in desktop Environments

--- src/quo.events.gestures.js
+++ src/quo.events.gestures.js
@@ -70,7 +70,9 @@
             TOUCH.x1 = TOUCH.x2 = TOUCH.y1 = TOUCH.y2 = TOUCH.last = 0;
             TOUCH = {};
         } else {
-            TOUCH.el.trigger('tap');
+            if (TOUCH.el !== undefined) {
+                TOUCH.el.trigger('tap');
+            }
             TOUCH_TIMEOUT = setTimeout(function(){
                 TOUCH_TIMEOUT = null;
                 TOUCH = {};
