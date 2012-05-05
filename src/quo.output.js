d897c5e4521aae2341fea9626db82d4b9ce3db19
@soyjavi <javi@tapquo.com>
2012-05-05 12:51:17 +0200

    in html() accept 'string' values and 'number' values

--- src/quo.output.js
+++ src/quo.output.js
@@ -22,7 +22,7 @@
      * ?
      */
     $$.fn.html = function(value) {
-        return ($$.toType(value) === 'string') ?
+        return ($$.toType(value) === 'string' || $$.toType(value) == 'number') ?
             this.each(function() {
                 this.innerHTML = value;
             })
