b1741940c571d7fbbd486644d53863612cb14972
Javi Jiménez <javi@tapquo.com>
2012-05-22 13:58:38 +0200

    Easy tests

---
+++ vendor/test/test.element.js
@@ -0,0 +1,13 @@
+// ELEMENT
+el = $$('input');
+el.attr('placeholder', 'Dinamic placeholder setted');
+el.val('Hello world');
+console.log('The input value is ' + el.val());
+el.val('');
+el.data('date', '1980');
+console.log('The data-date value is ' + el.data('date'));
+$$('#box-show').show();
+$$('#box-hide').hide();
+console.error('#box-show offset is ', $$('#box-show').offset());
+console.error('#box-hide offset is ', $$('#box-hide').offset());
+$$('#element').addClass('success');
---
+++ vendor/test/test.output.js
@@ -0,0 +1,11 @@
+// OUTPUT
+$$('#box-delete').empty();
+console.log('Delete text from output box');
+
+var el = $$('#box-input');
+el.text('First insert: text');
+el.append('<article>Append a element</article>');
+el.prepend('<article>Prepend a element</article>');
+html_of_last_article = el.children('article:last-child').html();
+el.children('article:last-child').html(html_of_last_article + '>> added a portion of html :)')
+$$('#output').addClass('success');
