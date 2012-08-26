9a081347bf8d8f6999abb533370e2bea845da310
@soyjavi <javi@tapquo.com>
2012-08-26 13:33:36 +0200

    Subselector enable in $$(selector, children)

--- vendor/test/test.element.js
+++ vendor/test/test.element.js
@@ -1,4 +1,4 @@
-// ELEMENT
+
 el = $$('input');
 el.attr('placeholder', 'Dinamic placeholder setted');
 el.val('Hello world');
@@ -11,3 +11,8 @@ $$('#box-hide').hide();
 console.error('#box-show offset is ', $$('#box-show').offset());
 console.error('#box-hide offset is ', $$('#box-hide').offset());
 $$('#element').addClass('success');
+
+
+childs = $$('#father', 'article');
+childs.last().addClass('success').html('Owned Son')
+console.error("childs ->", childs);
